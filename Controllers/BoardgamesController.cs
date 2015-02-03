257abd71f4bdc75243f707155989cc60e5ad2a35
Kyle Sweet <ksweet007@gmail.com>
2015-02-03 15:42:06 -0500

    Commit
    Commi

---
+++ Controllers/BoardgamesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BoardgameNight.Core.Interfaces;
+using BoardgameNight.Core.Model;
+using BoardgameNight.Infrastructure.Data;
+
+namespace BoardGameNight.Controllers
+{
+    public class BoardgamesController : Controller
+    {
+
+        private readonly IBoardGameRepository _boardGameRepository;
+
+        public BoardgamesController(IBoardGameRepository boardGameRepository)
+        {
+            _boardGameRepository = boardGameRepository;
+        }
+
+        public BoardgamesController() :
+            this(new BoardgameRepository())
+        {
+        }
+
+        public ActionResult Index()
+        {
+            var games = _boardGameRepository.ListGames();
+            
+            return View(games);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Boardgame
+            {
+                BoardgameName = "Test",
+                PlayerCount = 2,
+                HasBeenPlayed = true
+            });
+        }
+    }
+}
